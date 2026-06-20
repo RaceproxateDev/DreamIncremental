0656d4348bd312d8b1a5bda0a31fa0d95eb1b913
RaceproxateDev <coxemeftigtipout@gmail.com>
2026-06-20 15:37:29 +0200

    a

---
+++ GameScripts/World_2/Nightmares.js
@@ -0,0 +1,18 @@
+let NightmareReqTxt = document.getElementById("NightmareReqTxt");
+let NightmareResetBtn = document.getElementById("NightmareResetBtn");
+
+function calcNightmareReq() {
+    let req = new OmegaNum(1e56);
+
+    return req;
+}
+
+function updateNightmareHtml() {
+    NightmareReqTxt.innerHTML = `You need ${format(calcNightmareReq())} Void`;
+    NightmareResetBtn.innerHTML = (Data.Void.gte(1e56)) ? "Nightmare" : "Not enough Void";
+}
+
+setInterval(() => {
+    calcNightmareReq();
+    updateNightmareHtml();
+}, 100);
